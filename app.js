7a3e693520d90d7592c403b85fc68327fe7dc8ba
Binhleesin <ngodangbinh542002@gmail.com>
2022-09-07 10:22:40 +0700

    update

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-$('.slide__item-mid').slick({
+AOS.init();
+
+$('.slide__img').slick({
   autoplay: true,
   autoplaySpeed: 2000,
-  dots:true,
+  // arrows: false,
   prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
   nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
 });
 
-$('.slide__show').slick({
+
+
+$('.slide__show-all').slick({
   centerMode: true,
   centerPadding: '60px',
   slidesToShow: 3,
   variableWidth: true,
-  autoplay: true,
+  // autoplay: true,
   autoplaySpeed: 2000,
+  arrows: false,
+  slidesToScroll:1,
+  dots: true,
   prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
   nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
   responsive: [
     {
-      breakpoint: 740,
+      breakpoint: 1023,
       settings: {
         arrows: false,
         centerMode: true,
         centerPadding: '40px',
+        variableWidth: true,
         slidesToShow: 3
       }
     },
@@ -37,17 +45,6 @@ $('.slide__show').slick({
   ]
 });
 
-// $('.slide__image-right').slick({
-//   dots: false,
-//   infinite: true,
-//   speed: 500,
-//   fade: true,
-//   cssEase: 'linear',
-//   autoplay: true,
-//   autoplaySpeed: 2000,
-//   prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
-//   nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
-// });
 
 $('.slide__image-right').slick({
   autoplay: true,
@@ -55,4 +52,39 @@ $('.slide__image-right').slick({
   // dots:true,
   prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
   nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
+});
+
+$('.feedback__mid-all').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  // dots:true,
+  prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
+  nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
+});
+
+$('.list__news-option').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
+  nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
+    responsive: [
+    {
+      breakpoint: 1023,
+      settings: {
+        arrows: false,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
