94b1eba1b7e1451db01bbf303c5d4b07aa6e8c27
Binhleesin <ngodangbinh542002@gmail.com>
2022-09-10 15:13:57 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 AOS.init();
 
 $('.slide__img').slick({
-  autoplay: true,
+  // autoplay: true,
   autoplaySpeed: 2000,
   // arrows: false,
   prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
@@ -12,10 +12,11 @@ $('.slide__img').slick({
 
 $('.slide__show-all').slick({
   centerMode: true,
-  centerPadding: '60px',
+  centerPadding: '0px',
+  speed: 1500,
   slidesToShow: 3,
   variableWidth: true,
-  // autoplay: true,
+  autoplay: true,
   autoplaySpeed: 2000,
   arrows: false,
   slidesToScroll:1,
@@ -24,13 +25,13 @@ $('.slide__show-all').slick({
   nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
   responsive: [
     {
-      breakpoint: 1023,
+      breakpoint: 740,
       settings: {
         arrows: false,
         centerMode: true,
         centerPadding: '40px',
         variableWidth: true,
-        slidesToShow: 3
+        slidesToShow: 2
       }
     },
     {
@@ -46,35 +47,44 @@ $('.slide__show-all').slick({
 });
 
 
-$('.slide__image-right').slick({
+$('.list__slide-show').slick({
   autoplay: true,
   autoplaySpeed: 2000,
-  // dots:true,
+  // arrows: false,
+  speed: 1500,
+  // variableWidth: true,
   prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
   nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
+});
+
+$('.conference-slide-image').slick({
+  arrows: false,
+  dots:true,
 });
 
 $('.feedback__mid-all').slick({
   autoplay: true,
   autoplaySpeed: 2000,
-  // dots:true,
+  dots:true,
+  arrows: false,
+  // variableWidth: true,
   prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
   nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
 });
 
 $('.list__news-option').slick({
   slidesToShow: 3,
-  slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
+  // arrows: false,
   prevArrow: '<button class="chevron-prev"><i class="fas fa-chevron-left"></i></button>',
   nextArrow: '<button class="chevron-next"><i class="fas fa-chevron-right"></i></button>',
-    responsive: [
+  responsive: [
     {
-      breakpoint: 1023,
+      breakpoint: 740,
       settings: {
         arrows: false,
-        centerPadding: '40px',
+        // variableWidth: true,
         slidesToShow: 2
       }
     },
@@ -82,7 +92,6 @@ $('.list__news-option').slick({
       breakpoint: 480,
       settings: {
         arrows: false,
-        centerPadding: '40px',
         slidesToShow: 1
       }
     }
